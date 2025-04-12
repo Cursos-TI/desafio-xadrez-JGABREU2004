c64e394d144cec6519d8b31b1d4cbdd0bd6340fb
JGABREU2004 <203727025+JGABREU2004@users.noreply.github.com>
2025-04-12 01:00:32 +0000

    Finalizando movimento da Torre

--- xadrez.c
+++ xadrez.c
@@ -10,12 +10,14 @@ int main() {
     int bispo;
     int rainha;
     int opcao;
+    int i;
 
     printf("### Jogo Xadrez ###\n");
-    printf("Escolha a peça para movimentar: ");
+    printf("\nEscolha a peça para movimentar\n");
     printf("\n1. Torre\n");
     printf("2. Bispo\n");
     printf("3. Rainha\n");
+    printf("\nDigite a opção: ");
     scanf("%d", &opcao);
 
 switch (opcao)
@@ -24,14 +26,27 @@ case 1:
     printf("Digite a quantidade de movimento da Torre: ");
     scanf("%d", &torre);
 
-    // mover a Torre 5 casas para frente
-    for (torre = 0; torre < 5; torre++)
+    // mover a Torre para frente
+    for (i = 0; i < torre; i++)
 {
-    printf("Frente\n"); // imprime a direção do movimento
+    printf("Frente\n", i + 1); // imprime a direção do movimento
 }
     break;
 case 2:
-    printf("Teste\n");
+    printf("Digite a quantidade de movimento do Bispo: ");
+    scanf("%d", &bispo);
+
+    while (bispo <= 5)
+    { 
+        // mover o Bispo 5 casas para diagonal (direita/frente)
+        if (bispo >= 0) {
+        printf("Direita / frente");
+        bispo++;
+        }
+        else {
+            printf("Movimento inválido!");
+        }
+    }
     break;
 case 3:
     printf("Teste\n");
