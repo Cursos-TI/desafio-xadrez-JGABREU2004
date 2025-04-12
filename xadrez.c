56b84b30a1d218cb55ad2f09bdb3c009b9e0e97d
JGABREU2004 <203727025+JGABREU2004@users.noreply.github.com>
2025-04-12 19:14:51 +0000

    Ajustando Código para escolher a direção

--- xadrez.c
+++ xadrez.c
@@ -9,6 +9,7 @@ int main() {
     // criando as variáveis do jogo.
 
     int torre, bispo, rainha, opcao, i;
+    char direcao;
 
     // Menu interativo.
 
@@ -25,31 +26,112 @@ int main() {
 
 switch (opcao)
 {
-case 1:
-    // movimento da Torre.
+case 1: // movimento da Torre.
 
+    printf("\n** Direção do movimento **\n");
+    printf("D. Direita\n");
+    printf("E. Esquerda\n");
+    printf("C. Cima\n");
+    printf("B. Baixo\n");
+    printf("\nDigite a direção: ");
+    scanf(" %c", &direcao);
     printf("\nDigite a quantidade de movimento da Torre: ");
-    scanf("%d", &torre);
+    scanf("%d", &torre);    
 
-    for (i = 0; i < torre; i++) // define o valor de i, depois a comparação e finaliza com o incremento.
-{
-    printf("Cima\n", i + 1); // imprime a direção do movimento.
-}
-    break;
-case 2:
-    // movimento do Bispo para diagonal.
-
-    printf("\nDigite a quantidade de movimento do Bispo: ");
-    scanf("%d", &bispo);
-
-    i = 0; // definindo o valor de i.
-
-    while (i < bispo) // comparação, quando a condição for falsa finaliza o loop.
-    { 
-        printf("Direita / Cima\n", i + 1); // imprime a direção do movimento.
-        i++;
+    switch (direcao) // direção e quantidade de movimento da Torre.
+    {
+    case 'D':
+    case 'd':
+        for (i = 0; i < torre; i++) // define o valor de i, depois a comparação e finaliza com o incremento.
+    {
+            printf("Direita\n"); // imprime o movimento para direita.
     }
     break;
+    case 'E':
+    case 'e':
+        for (i = 0; i < torre; i++) // define o valor de i, depois a comparação e finaliza com o incremento.
+    {
+            printf("Esquerda\n"); // imprime o movimento para esquerda.
+    }
+    break;
+    case 'C':
+    case 'c':
+        for (i = 0; i < torre; i++) // define o valor de i, depois a comparação e finaliza com o incremento.
+    {
+            printf("Cima\n"); // imprime o movimento para cima.
+    }
+    break;
+    case 'B':
+    case 'b':
+        for (i = 0; i < torre; i++) // define o valor de i, depois a comparação e finaliza com o incremento.
+    {
+            printf("Baixo\n"); // imprime o movimento para baixo.
+    }
+    break;
+    default:
+        printf("Direção inválida\n");
+    break;
+    }
+break;
+case 2: // movimento do Bispo para diagonal.
+
+    printf("\n** Direção do movimento **\n");
+    printf("A. Direita/Baixo\n");
+    printf("B. Esquerda/Baixo\n");
+    printf("C. Direita/Cima\n");
+    printf("D. Esquerda/Cima\n");
+    printf("\nDigite a direção: ");
+    scanf(" %c", &direcao);
+    printf("\nDigite a quantidade de movimento do Bispo: ");
+    scanf("%d", &bispo);    
+
+    switch (direcao) // direção e quantidade de movimento do Bispo.
+    {
+    case 'A':
+    case 'a':
+        i = 0; // definindo o valor de i.
+
+        while (i < bispo) // comparação, quando a condição for falsa finaliza o loop.
+    { 
+            printf("Direita / Baixo\n"); // imprime a direção do movimento.
+            i++;
+    }
+    break;
+    case 'B':
+    case 'b':
+        i = 0; // definindo o valor de i.
+
+        while (i < bispo) // comparação, quando a condição for falsa finaliza o loop.
+    { 
+            printf("Esquerda / Baixo\n"); // imprime a direção do movimento.
+            i++;
+    }
+    break;
+    case 'C':
+    case 'c':
+        i = 0; // definindo o valor de i.
+
+        while (i < bispo) // comparação, quando a condição for falsa finaliza o loop.
+    { 
+            printf("Direita / Cima\n"); // imprime a direção do movimento.
+            i++;
+    }
+    break;
+    case 'D':
+    case 'd':
+        i = 0; // definindo o valor de i.
+
+        while (i < bispo) // comparação, quando a condição for falsa finaliza o loop.
+    { 
+            printf("Esquerda / Cima\n"); // imprime a direção do movimento.
+            i++;
+    }
+    break;
+    default:
+        printf("Direção inválida\n");
+    break;
+    }
+break;
 case 3:
     // movimento da Rainha.
 
@@ -69,6 +151,8 @@ case 3:
 case 4:
 
     // movimento do Cavalo em L.
+
+    printf("Movimento do Cavalo em L\n");
 
     int cavalo = 1; // flag para controlar o movimento em L
 
